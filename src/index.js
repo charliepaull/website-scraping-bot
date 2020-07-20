a3f3a4a14d9262a95e0f794a5e23e083cbe0abc1
charliepaull <charliepaull211@gmail.com>
2020-07-20 13:38:07 -0700

    housing bot. need to figure out bitly shortened link. first commit to repo.

---
+++ src/index.js
@@ -0,0 +1,119 @@
+require("dotenv").config();
+const requestPromise = require("request-promise");
+const checksum = require("checksum");
+const cheerio = require("cheerio");
+
+// require bitly
+const { BitlyClient } = require('bitly');
+// create new instance of bitly to be used in application, using my access token 
+const bitly = new BitlyClient(process.env.BITLY_AUTH_TOKEN, {}); 
+
+// use my own credentials for this one - put this in a .env file later before going on GitHub
+const client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
+// Zillow URL: add url here through process.env
+const zillowURL = process.env.ZILLOW_URL_LA;
+
+let bitlyURL;
+
+// from bitly npm package documentation
+async function init() {
+    try {
+      bitlyURL = await bitly.shorten(zillowURL);
+    } catch (error) {
+      throw error;
+    }
+    return bitlyURL;
+}
+  
+// calling method to create short bitly link
+init();
+// console.log(bitlyURL.link) // this isn't defined in scope
+/*
+Ok great, we’re getting a list of unique ID numbers. These ID’s are the only information we care about on the page.
+So let’s go back to our original plan of comparing hashes, except we’ll only hash the unique IDs:
+IDs are logged out on line 21
+*/
+
+// declare a variable hash so we can save its value
+let hash = "";
+
+// checking the site URL and creating a hash using checksum package
+function checkURL(siteToCheck) {
+    
+    return requestPromise(siteToCheck)
+        .then(HTMLresponse => {
+
+            const $ = cheerio.load(HTMLresponse);
+            let apartmentString = "";
+            
+            $(".list-card.list-card-short").each((i, element) => {
+                // gets me all the IDs of the ads on Zillow from url page.
+                apartmentString += `${element.attribs.id}`
+            });
+
+            // now, check if the hash is the same as the previous hash
+
+            // if hash variable is empty, set apartmentString as a hash equal to variable, hash.
+            if (hash === "") {
+                console.log("Making initial fetch...");
+                hash = checksum(apartmentString);
+            }
+
+            // if hash variable and checksum(apartmentString) are NOT the same - return true, send text alert.
+            if (checksum(apartmentString) !== hash) {
+                // set hash variable to updated apartmentString
+                hash = checksum(apartmentString);
+                return true;
+            }
+
+            // else, no changes, return false
+            console.log("No updated postings yet.");
+            return false;
+        })
+        .catch(error => {
+            console.log(`Could not complete fetch of ${zillowURL}: ${error}`);
+        });
+};
+
+// Twilio API SMS Messaging part
+function SMS({ body, to, from }) {
+    client.messages.create({
+        body,
+        to,
+        from
+    }).then(() => {
+        console.log(`👍 Success! Message has been sent to ${to}`);
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+
+
+// check every 10 seconds
+// doing this asynchonously so our fetch is sure to resolve
+setInterval(async () => {
+    // if checkURL is true, send a text message!
+    if (await checkURL(zillowURL)) {
+        console.log("Found a change! Sending updated text now..")
+
+        SMS({
+            body: `Look at these apartment listings! ${bitlyURL}`,
+            to: process.env.CP_NUMBER,
+            from: process.env.TWILIO_NUMBER
+        });
+    }
+}, 10000);
+
+
+// // Download the helper library from https://www.twilio.com/docs/node/install
+// // Your Account Sid and Auth Token from twilio.com/console
+// // DANGER! This is insecure. See http://twil.io/secure
+// const accountSid = 'AC08113eabf96c276230bf1c0919ba2d2d';
+// const authToken = '578fe428921304d7eefcb4df96385a83';
+// const client = require('twilio')(accountSid, authToken);
+
+// client.messages
+//       .create({body: `Matt Asher has a skinny belly at ${zillowURL}, tehe.`, from: '+13104608664', to: '+13104608664'})
+//       .then(message => console.log(message.sid));
